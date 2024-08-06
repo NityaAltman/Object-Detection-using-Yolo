0a602fb438f7b0ff6c532a2e4175deab8e54ff40
Suon Nityaprathanu <suonnityaloogip@gmail.com>
2024-08-06 17:32:54 +0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Nitya Altman',
+    author_email= 'suonnityaloogip@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
